f123b52414ceff620f96beff141cc782bf93a18d
emiliosutti <ejmsutti@gmail.com>
2019-02-08 09:48:44 -0300

    installers

---
+++ wasekw1.0/installers/install_linux64.pl
@@ -0,0 +1,12 @@
+# installer for Linux 64
+
+system "cpan Data::Dumper";
+system "cpan Tie::IxHash";
+system "cpan Tie::IxHash::Easy";
+system "cpan Storable";
+system "apt-get install libtk-perl";
+system "cpan Tk::Tree";
+system "apt-get install libtkx-perl";
+system "cpan JSON";
+system "cpan JSON::Parse";
+system "cpan List::MoreUtils";
