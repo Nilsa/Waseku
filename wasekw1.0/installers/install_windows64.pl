06e90601c54dada22fa5f6a73db7830794311117
emiliosutti <ejmsutti@gmail.com>
2019-02-08 10:18:55 -0300

    installers

---
+++ wasekw1.0/installers/install_windows64.pl
@@ -0,0 +1,12 @@
+# installer for Windows 64
+
+system "cpan Data::Dumper";
+system "cpan Tie::IxHash";
+system "cpan Tie::IxHash::Easy";
+system "cpan Storable";
+system "ppm install libtk-perl";
+system "cpan Tk::Tree";
+system "ppm install libtkx-perl";
+system "cpan JSON";
+system "cpan JSON::Parse";
+system "cpan List::MoreUtils";
