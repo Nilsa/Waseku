de317d1ca2dc026b7307d35d3c01b6aa7db3f3bb
emsutti <ejmsutti@gmail.com>
2019-06-11 13:09:50 -0300

    objects

---
+++ wasekw1.0/src/hybridtree.pm
@@ -0,0 +1,13 @@
+#!usr/bin/env perl
+
+package hybridtree;
+use strict;
+use warnings;
+use node;
+use otree;
+
+our @ISA = qw( node );
+our @GWC = qw( otree );
+
+
+
--- wasekw1.0/src/otree.pm
+++ wasekw1.0/src/otree.pm
@@ -3,8 +3,7 @@
 package otree;
 use strict;
 use warnings;
-use node;
-our @ISA = qw( node );
+use Tie::IxHash::Easy;
 
 sub new {
     my $class = shift;
@@ -20,20 +19,73 @@ sub new {
 
 sub empty_ont_tree {
     my ($_ont_tree) = @_;
-
-    return $_ont_tree->{_ont_tree} = {};
+    return $_ont_tree->{_ont_tree} = %_;
 }
 
 sub empty_def_list {
     my ($_ont_tree) = @_;
-
     return $_ont_tree->{_def_list} = @_;
 }
 
 sub empty_fig_path {
     my ($_ont_tree) = @_;
-
     return $_ont_tree->{_fig_path} = "";
+}
+
+sub hash_creator{
+    my ($fh) = @_;
+    my $contents = do { local(@ARGV, $/) = $fh; <> } or return;
+    my @lines = split '\n', $contents;
+    my @stack;
+    tie my %hash, 'Tie::IxHash::Easy';
+    push @stack, \%hash;
+    foreach(@lines){
+        chomp;
+        s/^(\t*)//;
+        splice @stack, length($1)+1;
+        push @stack, $stack[$#stack]->{$_} = {};
+    }
+    return %hash;
+}
+
+sub list_converter{
+    my ($fh) = @_;
+    open PFILE, '<', $fh;
+    local $/ = '##';
+    my @paragraph;
+    while (<PFILE>){
+        chomp;
+        s/[\$(\n*)]//;
+        push @paragraph, "$_";
+    }
+    close PFILE;
+    return @paragraph;
+}
+
+
+sub populate_ont_tree {
+    my ($self, $filename) = @_;
+
+    my %tmp = hash_creator($filename);
+    $self->{_ont_tree} = %tmp if defined(%tmp);
+    return $self->{_ont_tree};
+    undef %tmp;
+}
+
+sub insert_def_list {
+    my ($self, $filename) = @_;
+
+    my @tmp = list_converter($filename);
+    $self->{_def_list} = @tmp if defined(@tmp);
+    return $self->{_def_list};
+    undef @tmp;
+}
+
+sub set_fig_path {
+    my ($self, $figpath) = @_;
+
+    $self->{_fig_path} = $figpath if defined($figpath);
+    return $self->{_fig_path};
 }
 
 sub read_ont_tree {
